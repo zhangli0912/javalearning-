03adf96ecc4d66d3003e65d6ce5e7ffa5307f16b
zhangli0912 <834429823@qq.com>
2015-08-30 22:18:59 +0800

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,13 @@
+public class Sum {
+	public static void main(String[] args){
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		int sum=0;
+		int i=1;
+		while(i<=100){
+			sum=sum+i;
+			i++;
+		}
+		System.out.println(sum);
+	}
